0c8f16d5dd7fa217f3c8b832362826cad4ed0666
Aderito D Maciel <vendas@vendernanet.com.br>
2026-05-09 12:42:10 -0300

    ecommerce.tsx

--- artifacts/saas-mobile/app/cliente/ecommerce.tsx
+++ artifacts/saas-mobile/app/cliente/ecommerce.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, Text, StyleSheet, ScrollView, Pressable, useColorScheme, Platform, Alert, ActivityIndicator, Image, TextInput } from "react-native";
 import PixPagamento from "@/components/PixPagamento";
 import SegmentoBottomNav, { SEGMENTO_NAV_HEIGHT } from "@/components/SegmentoBottomNav";
